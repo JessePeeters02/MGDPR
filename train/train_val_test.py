43ba526e9bf6c977b4cfe234e4087b849b8dbd0c
JessePeeters02 <jesse.peeters@student.kuleuven.be>
2025-02-12 11:00:57 +0000

    Feb 12, 2025, 12:00 PM

--- train/train_val_test.py
+++ train/train_val_test.py
@@ -2,6 +2,10 @@ import torch
 import csv as csv
 import torch.nn.functional as F
 import torch.distributions
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from MGDPR.graph_dataset_gen import MyDataset
 from MGDPR.dataset.graph_dataset_gen import Mydataset
 from Multi_GDNN import MGDPR
 from sklearn.metrics import matthews_corrcoef, f1_score
